1e1c597ded58d310d66a0496a75b8a8e973b8054
fuckingshit25 <103372417+fuckingshit25@users.noreply.github.com>
2023-11-08 16:30:30 +0700

    Add files via upload

---
+++ ClassLibrary_forDB/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClassLibrary_forDB
+{
+    public class User
+    {
+        [Key]
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string message { get; set; }
+    }
+}
